a5bd32850d85e8058726d9d3d7ce1b63631ef165
kamilkow1123 <22kamil.kow@gmail.com>
2021-07-16 13:51:42 +0200

    Draft of the homepage

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
 import React from "react";
+import { HashRouter as Router, Switch, Route } from "react-router-dom";
+import Home from "./components/Home";
 
 const App = () => {
     return (
-        <div>
-            <h1>Hello</h1>
-        </div>
+        <Router>
+            <Switch>
+                <Route path="/" component={Home} exact />
+            </Switch>
+        </Router>
     );
 };
 
