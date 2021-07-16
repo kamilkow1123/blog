91502caa64488f5e539818da9b7cef222a29faca
kamilkow1123 <22kamil.kow@gmail.com>
2021-07-16 17:15:06 +0200

    Adding action types

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,7 +1,8 @@
 import jsonPlaceholder from "../apis/jsonPlaceholder";
+import { FETCH_POSTS } from "./types";
 
 export const fetchPosts = () => async dispatch => {
     const response = await jsonPlaceholder.get("/posts");
 
-    dispatch({ type: "FETCH_POSTS", payload: response });
+    dispatch({ type: FETCH_POSTS, payload: response });
 };
